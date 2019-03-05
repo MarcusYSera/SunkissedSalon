709f3e4f30442b5c33dc56c6c3888619a73c690c
Marcus Sera <m.yoshiosera@gmail.com>
2019-03-05 12:15:59 -0800

    commented out model

--- apps/salon_home/models.py
+++ apps/salon_home/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+# class Employee(models.Model):
+#     first_name = models.CharField(max_length=255)
+#     last_name = models.CharField(max_length=255)
+#     image = models.TextField()
+#     instaLink = models.TextField()
+#     bio = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add = True)
+#     updated_at = models.DateTimeField(auto_now = True)
